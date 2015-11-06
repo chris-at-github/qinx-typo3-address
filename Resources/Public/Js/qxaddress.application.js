b2d1e1b4b13e861ef54ee13074541faaa50ee44a
Christian Pschorr <c.pschorr@datamints.com>
2015-11-06 12:59:56 +0100

    Implement basic javascript for markers

---
+++ Resources/Public/Js/qxaddress.application.js
@@ -0,0 +1,54 @@
+$(function() {
+	$('.qx-address--map').each(function() {
+		var container = $(this).find('.qx-address--map-container');
+		var options = {
+			zoom: {
+				default: 9,
+				max: 15
+			}
+		};
+
+		var map = new google.maps.Map(container[0], {
+			center: {
+				lat: 47.636896,
+				lng: 11.231763
+			},
+			zoom: 15
+		});
+
+		var markers = [];
+		var bound		= new google.maps.LatLngBounds();
+
+		$(this).find('.qx-address--map-marker').each(function() {
+			var item 			= $(this);
+			var latitude	= parseFloat(item.data('qxLatitude'));
+			var longitude	= parseFloat(item.data('qxLongitude'));
+			var position	= new google.maps.LatLng(latitude, longitude)
+
+			var marker		= new google.maps.Marker({
+				position: position
+			});
+
+			markers.push(marker);
+			bound.extend(position);
+		});
+
+		if(markers.length !== 0) {
+			for(var i = 0; i < markers.length; i++) {
+				markers[i].setMap(map);
+			}
+
+			map.fitBounds(bound);
+
+			if(typeof(options.zoom.max) !== 'undefined') {
+				var listener = google.maps.event.addListener(map, 'idle', function() {
+					if(map.getZoom() > options.zoom.max) {
+						map.setZoom(options.zoom.max);
+					}
+
+					google.maps.event.removeListener(listener);
+				});
+			}
+		}
+	});
+});
